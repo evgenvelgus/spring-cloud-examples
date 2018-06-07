f8a61b73382a4a8b2145573fc83fd3e272e9e33c
evgenvelgus <evgen.velgus@kenshoo.com>
2018-06-07 13:30:29 +0300

    small fixes

--- account-service/src/main/java/com/demo/account/controllers/AccountController.java
+++ account-service/src/main/java/com/demo/account/controllers/AccountController.java
@@ -1,17 +1,31 @@
 package com.demo.account.controllers;
 
 import com.demo.account.domains.Account;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class AccountController {
 
+    private RestTemplate restTemplate = new RestTemplate();
 
+    protected static final String FIELDS = "?fields=";
 
-    @RequestMapping(method = RequestMethod.GET)
-    public Account getAccount() {
+    protected static final String AD_ACCOUNT_FIELDS = "account_id,id,name,account_status,currency,timezone_id,"
+            + "timezone_name,age,business_country_code";
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+    public Account getAccount(@PathVariable String id) {
+       // restTemplate.getForObject(uri, type);
+
         return new Account("act_359773042", "359773042", "aaa@com");
+    }
+
+
+    private String getAccountFields() {
+        return FIELDS + AD_ACCOUNT_FIELDS;
     }
 }
